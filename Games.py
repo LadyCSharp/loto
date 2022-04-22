e1a7532e78949e295af7094d6b17305c4278fdd7
Mary True <mat-vsuwt@ya.ru>
2022-04-22 10:14:36 +0300

    PC

--- Games.py
+++ Games.py
@@ -1,14 +1,19 @@
 from meshok import Meshok
 from Players import Player
+
+
 class Game(object):
     def __init__(self):
-        self.etap=0
-        self.gameOver=False 
-        self.m=Meshok()
-        p1=Player('Mary','Human')
-        p2=Player('lenovo','Comp')
-        p3=Player('Borland','Cat')
-        self.players=[p1,p3,p2]       
+        self.etap = 0
+        self.gameOver = False
+        self.m = Meshok()
+        # p1=Player('Mary','Human')
+        # p2=Player('lenovo','Comp')
+        # p3 = Player('Borland', 'Cat')
+        p1 = Player('Mary', 'Comp')
+        p2 = Player('lenovo', 'Comp')
+        p3 = Player('Borland', 'Comp')
+        self.players = [p1, p3, p2]
 
     def show(self):
         print('Рaунд ', self.etap, '\n')
@@ -18,22 +23,20 @@ class Game(object):
             print('Игра закончена')
 
     def step(self):
-        self.etap+=1
-        b=self.m.get()
+        self.etap += 1
+        b = self.m.get()
         print('Выпал боченок', b)
-        rez=[]
+        # rez=[]
         for p in self.players:
-            r=p.cross_out(b)
-            if r=='win':
+            r = p.cross_out(b)
+            if r == 'win':
                 print('Выиграл', p.name)
-                self.gameOver=True
-            if r=='loose':
+                self.gameOver = True
+            if r == 'loose':
                 print('Проиграл', p.name)
-                self.gameOver=True
-            #rez.append(p.cross_out(b))
-        #print (rez)
-        #self.gameOver= self.m.isEmpty()
+                self.gameOver = True
+            # rez.append(p.cross_out(b))
+        # print (rez)
+        # self.gameOver= self.m.isEmpty()
         if self.m.isEmpty():
-            self.gameOver=True
-
-
+            self.gameOver = True
